acf840c00c88b15a45866936cf2d28c50b84d9c5
Singamala Dhanush Reddy <dhanushreddy.1514@gmail.com>
2024-07-09 12:13:05 +0530

    first commit related , to input output file setup for easy usage.

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    int y = 20;
+    cin >> x;
+    cout << "x value is = " << x << ",y is =" << y;
+    return 0;
+}
+
+
---
+++ temp.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    int y = 88;
+    cin >> x;
+    cout << "x value is = " << x << ",y is =" << y;
+    return 0;
+}
